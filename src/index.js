e49dd1b75866a83545aa26a32698830b17b46bb1
Vlad Tregubov <u.trehubau@gmail.com>
2022-02-06 15:20:58 +0100

    Redux implementation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import { createStore } from './createStore';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import { rootReducer } from './redux/rootReducer';
+import { asyncIncrement, changeTheme, decrement, increment } from './redux/actions';
+
 import './styles.css';
 
 const counter = document.getElementById('counter');
@@ -8,31 +13,32 @@ const subBtn = document.getElementById('sub');
 const asyncBtn = document.getElementById('async');
 const themeBtn = document.getElementById('theme');
 
-const store = createStore(rootReducer, 0);
-
-window.store = store;
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 addBtn.addEventListener('click', () => {
-	store.dispatch({ type: 'INCREMENT' });
+	store.dispatch(increment());
 });
 
 subBtn.addEventListener('click', () => {
-	store.dispatch({ type: 'DECREMENT' });
+	store.dispatch(decrement());
 });
 
 asyncBtn.addEventListener('click', () => {
+	store.dispatch(asyncIncrement());
+});
 
+themeBtn.addEventListener('click', () => {
+	const newTheme = document.body.classList.contains('light')
+		? 'dark'
+		: 'light';
+	store.dispatch(changeTheme(newTheme));
 });
 
 store.subscribe(() => {
 	const state = store.getState();
 
-	counter.textContent = state;
+	counter.textContent = state.counter;
+	document.body.className = state.theme.value;
 });
 
 store.dispatch({ type: 'INIT_APPLICATION' });
-
-
-themeBtn.addEventListener('click', () => {
-
-});
